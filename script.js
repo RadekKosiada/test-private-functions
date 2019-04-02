387da3a498df432eac1ad93dc4e7798ad0db0f0e
RadekKosiada <kosiadar@gmail.com>
2019-04-02 18:43:38 +0200

    Add a new function with a private function inside

--- script.js
+++ script.js
@@ -2,21 +2,29 @@ const sum = (a, b) => a + b
 const mul = (a, b) => a * b
 
 
-var myModule = (function() {
-
+var myModule = (function () {
     function foo() {
-      // private function `foo` inside closure
-      return "foo"
+        // private function `foo` inside closure
+        return "foo"
     }
-  
     return {
-      bar: function(a,b) {
-        // public function `bar` returned from closure
-        return a+b;
-      }
+        bar: function (a, b) {
+            // public function `bar` returned from closure
+            return a + b;
+        }
     }
-  }())
-  
-  console.log(myModule.bar(3,2));
+}())
 
-  module.exports = { sum, mul, myModule }
+console.log(myModule.bar(3, 2));
+
+function abc() {
+    return {
+        def: function (arg) {
+            return arg + 'def';
+        }
+    }
+}
+
+console.log(abc.def("abc"))
+
+module.exports = { sum, mul, myModule }
--- script.test.js
+++ script.test.js
@@ -1,11 +1,13 @@
 const { sum, mul, myModule } = require('./script.js');
 
+console.log(myModule.bar)
+
 test('Adding 1 + 1 equals 2', () => {
     expect(sum(1, 1)).toBe(2)
 })
 test('Multiplying 1 * 1 equals 1', () => {
     expect(mul(1, 1)).toBe(1)
 })
-test('Adding 2 +3 equals 5'), () => {
-    expect(myModule(2,3)).toBe(5)
-}
+test('Adding 2 +3 equals 5', () => {
+    expect(myModule.bar(2,3)).toBe(5)
+})
